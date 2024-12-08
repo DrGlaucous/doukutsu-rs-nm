f3767a7080c726eb2351cd7a8b3cf6c9a6bbb780
Dr. Glaucous <49349519+DrGlaucous@users.noreply.github.com>
2024-12-07 21:29:16 -0500

    Enable 2P support for android retroarch
    By default, 2P is disabled for android in the upstream, I forgot to change the build condition to account for retroarch.

--- src/engine_constants/mod.rs
+++ src/engine_constants/mod.rs
@@ -289,7 +289,7 @@ impl EngineConstants {
             is_demo: false,
             supports_og_textures: false,
             has_difficulty_menu: true,
-            supports_two_player: cfg!(not(target_os = "android")),
+            supports_two_player: cfg!(any(not(target_os = "android"), feature = "backend-libretro")),
             game: GameConsts {
                 intro_stage: 72,
                 intro_event: 100,
